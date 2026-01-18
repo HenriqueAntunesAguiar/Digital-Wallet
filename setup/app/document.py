8513ca7c55ee6da9f56808f3bfc4330321d52142
HenriqueAntunesAguiar <hantunesaguiar@gmail.com>
2026-01-18 11:25:02 -0300

    update code for correct DDD

--- setup/app/document.py
+++ setup/app/document.py
@@ -1,4 +1,4 @@
-class NewTransaction():
+class DataModel():
     
     def __init__(self):
         self.balances = {
@@ -8,39 +8,42 @@ class NewTransaction():
 
         self.log = []
 
-    def TransactionValue(self, client_id_to_recive:str, client_id_to_discount:str, value_of_transaction:float):
-        
-        client_to_recive_current_balance = self.GetFakerBalance(client_id_to_recive)
-        client_to_discount_current_balance = self.GetFakerBalance(client_id_to_discount)
-
-        if self.ValidateTransactionAboutValueDiscount(client_to_discount_current_balance, value_of_transaction):
-            self.MakeTransaction(client_id_to_recive, client_to_recive_current_balance, client_id_to_discount, client_to_discount_current_balance, value_of_transaction)
-            return 'Success: The operation did normaly.'
-        else:
-            return 'Error: No balance to make a transaction.'
-
-
-    def ValidateTransactionAboutValueDiscount(self, current_value:float, value_to_discount:float):
-
-        if (current_value - value_to_discount) < 0:
-            return False
-        else:
-            return True
-
-    def GetFakerBalance(self, client_id):
-        return self.balances[client_id]
-
-    def MakeTransaction(self, client_id_to_recive:str, client_to_recive_current_balance:float, client_id_to_discount:str, client_to_discount_current_balance:float, value_of_transaction:float):
-
-        new_balance_of_client_to_recive = client_to_recive_current_balance + value_of_transaction
-        new_balance_of_client_to_discount = client_to_discount_current_balance - value_of_transaction
-
-        self.UpdateFakerBalance(client_id_to_recive, new_balance_of_client_to_recive)
-        self.UpdateFakerBalance(client_id_to_discount, new_balance_of_client_to_discount)
+    def GetFakerBalance(self, wallet_id):
+        return self.balances[wallet_id]
     
+    def UpdateFakerBalance(self, wallet_id:str, new_value_of_balance:float):
+        self.balances[wallet_id] = new_value_of_balance
 
-    
-    def UpdateFakerBalance(self, client_id:str, new_value_of_balance:float):
-
-        self.balances[client_id] = new_value_of_balance
         
+class Wallet:
+
+    def __init__(self, wallet_id, balance):
+       self.wallet_id = wallet_id
+       self.balance = balance
+
+    def debit(self, amount):
+        if self.balance - amount < 0:
+            raise Exception('Invalid amount')
+        
+        self.balance -= amount
+
+    def credit(self, amount):
+        self.balance += amount
+
+class Controller:
+    
+    def __init__(self):
+        self.model = DataModel()
+
+    def MakeTransaction(self):
+
+        client_1_balance = self.model.GetFakerBalance('1a')
+        client_1_wallet = Wallet('1a', client_1_balance)
+        client_1_wallet.debit(50.00)
+
+        client_2_balance = self.model.GetFakerBalance('2b')
+        client_2_wallet = Wallet('2b', client_2_balance)
+        client_2_wallet.credit(50.00)
+
+        self.model.UpdateFakerBalance(client_1_wallet.wallet_id, client_1_wallet.balance)
+        self.model.UpdateFakerBalance(client_2_wallet.wallet_id, client_2_wallet.balance)
